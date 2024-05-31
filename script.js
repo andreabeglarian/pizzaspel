d3e96c1540e4950c40e17905b7e0145783549103
andreabeglarian <145342589+andreabeglarian@users.noreply.github.com>
2024-05-31 15:52:28 +0200

    bron en js aangepast

--- script.js
+++ script.js
@@ -3,11 +3,9 @@ let buttonTomaten = document.getElementById("buttonTomaten")
 let sausImage = document.getElementById("saus")
 buttonTomaten.addEventListener("click", klik);
 
-
 let buttonKaas = document.getElementById("buttonKaas")
 let kaasoppizzaImage = document.getElementById("kaasoppizza")
 buttonKaas.addEventListener("click", klikKaas);
-
 
 let buttonOlijf = document.getElementById("buttonOlijven")
 let olijfImage = document.getElementById("olijfgesneden")
@@ -18,24 +16,20 @@ let uiImage = document.getElementById("uienringen")
 buttonUi.addEventListener("click", klikUi);
 
 let buttonSalami = document.getElementById("buttonSalami")
-let salamiImage = document.getElementById("plakjesalami")
 buttonSalami.addEventListener("click", klikSalami);
-
+let salamiImage = document.getElementById("plakjesalami")
 
 function klik() {
     sausImage.hidden = !sausImage.hidden;
 }
 
-
 function klikKaas() {
     kaasoppizzaImage.hidden = !kaasoppizzaImage.hidden;
 }
 
-
 function klikOlijf() {
     olijfImage.hidden = !olijfImage.hidden;
 }
-
 
 function klikUi() {
     uiImage.hidden = !uiImage.hidden;
